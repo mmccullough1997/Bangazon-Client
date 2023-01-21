e9737907635ec45fae90de4c63c864e84caa3b76
Mitch McCullough <mitchmccullough@gmail.com>
2023-01-21 10:53:22 -0600

    got a janky carousel

--- components/products/ProductCarousel.js
+++ components/products/ProductCarousel.js
@@ -1,7 +1,36 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import PropTypes from 'prop-types';
 
-export default function ProductCarousel() {
+export default function ProductCarousel({ shuffledProducts }) {
   return (
-    <div>ProductCarousel</div>
+    <Carousel>
+      {shuffledProducts.map((shuffledProduct) => (
+        <Carousel.Item key={shuffledProduct.id} className="carousel">
+          <img
+            className="d-block w-100"
+            src={shuffledProduct.image}
+            alt="First slide"
+            height="300"
+          />
+          <Carousel.Caption>
+            <h3>{shuffledProduct.title}</h3>
+            <p>A Bangazon Exclusive</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))}
+    </Carousel>
   );
 }
+
+ProductCarousel.propTypes = {
+  shuffledProducts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      image: PropTypes.string,
+      title: PropTypes.string,
+    }),
+  ).isRequired,
+};
